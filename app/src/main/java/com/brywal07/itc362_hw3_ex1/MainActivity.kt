a71db4b8ccdf459a821b4d1cb1ae2f376fd01672
brywal07 <60237935+brywal07@users.noreply.github.com>
2023-02-19 18:15:52 -0600

    first commit

--- app/src/main/java/com/brywal07/itc362_hw3_ex1/MainActivity.kt
+++ app/src/main/java/com/brywal07/itc362_hw3_ex1/MainActivity.kt
@@ -11,14 +11,16 @@ import com.brywal07.itc362_hw3_ex1.databinding.ActivityMainBinding
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var isClicked = false
     private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true))
+        Question(R.string.question_australia, true, false),
+        Question(R.string.question_oceans, true, false),
+        Question(R.string.question_mideast, false, false),
+        Question(R.string.question_africa, false, false),
+        Question(R.string.question_americas, true, false),
+        Question(R.string.question_asia, true, false))
     private var currentIndex = 0
+    private var currentQuestion = questionBank[currentIndex]
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,18 +28,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.trueButton.setOnClickListener { view: View ->
+            currentQuestion.isAnswered = true
             checkAnswer(true)
         }
         binding.falseButton.setOnClickListener { view: View ->
+            currentQuestion.isAnswered = true
             checkAnswer(false)
         }
         binding.nextButton.setOnClickListener{
             currentIndex = (currentIndex + 1) % questionBank.size
+            currentQuestion=questionBank[currentIndex]
             updateQuestion()
         }
         binding.previousButton.setOnClickListener{
             if(currentIndex > 0) {
                 currentIndex = (currentIndex - 1) % questionBank.size
+                currentQuestion=questionBank[currentIndex]
                 updateQuestion()
             }else{
                 Toast.makeText(this, "ERROR!", Toast.LENGTH_SHORT).show()
@@ -72,6 +78,8 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onDestroy(Bundle?) called)")
     }
     private fun updateQuestion(){
+        binding.trueButton.isEnabled = !currentQuestion.isAnswered
+        binding.falseButton.isEnabled = !currentQuestion.isAnswered
         val questionTextResId = questionBank[currentIndex].textResId
         binding.questionTextView.setText(questionTextResId)
     }
